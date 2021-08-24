e9c37d53a61f52a079c316237af65da06fd283a2
JorgeRuedaG <89436064+JorgeRuedaG@users.noreply.github.com>
2021-08-24 09:28:41 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+    x= float(input("Dame los minutos:"))
+    salida= (x*5.7*60)/10
+    print("Centimetros recorridos:",salida)
+    
     pass
 
 if __name__ == '__main__':
