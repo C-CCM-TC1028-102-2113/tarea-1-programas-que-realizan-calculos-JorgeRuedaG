f1550788af08222209a096c9bdba66aa41ae95f2
JorgeRuedaG <89436064+JorgeRuedaG@users.noreply.github.com>
2021-08-24 09:25:26 -0500

    Update exercise.py

--- assignments/22DigitosPares/src/exercise.py
+++ assignments/22DigitosPares/src/exercise.py
@@ -1,5 +1,29 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+
+    num = int(input("Dame un número:"))
+
+    flag = 0
+
+    if 10000 > num > 1000:
+
+        for i in range(4):
+            
+            if num % 2 == 0:
+                flag = flag +1
+                num = math.trunc(num / 10)
+                
+            elif num % 2 == 1:
+                flag = flag 
+                num = math.trunc(num / 10)
+                
+        print(flag)
+
+    else:
+        
+        print("invalid number")
+        
     pass
 
 
